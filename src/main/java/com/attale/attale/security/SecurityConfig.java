df9276410ac877785e942cb47a3df13c03aace58
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-10-26 19:25:06 +0530

    minor bug fixed in securityconfig file

--- src/main/java/com/attale/attale/security/SecurityConfig.java
+++ src/main/java/com/attale/attale/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.attale.attale.security;
 
+import com.attale.attale.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     @Autowired
-    private CachingUserDetailsService cachingUserDetailsService;
+    private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
