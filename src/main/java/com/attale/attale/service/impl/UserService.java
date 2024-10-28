fd4fe9c4b1331f00fef24c31d2a60eca6e8e43d7
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-10-28 21:35:14 +0530

    user service for login added

--- src/main/java/com/attale/attale/service/impl/UserService.java
+++ src/main/java/com/attale/attale/service/impl/UserService.java
@@ -2,13 +2,16 @@ package com.attale.attale.service.impl;
 
 import com.attale.attale.dto.LoginRequest;
 import com.attale.attale.dto.Response;
+import com.attale.attale.dto.UserDTO;
 import com.attale.attale.entity.User;
 import com.attale.attale.exception.OurException;
 import com.attale.attale.repo.UserRepository;
 import com.attale.attale.service.interfac.IUserService;
 import com.attale.attale.utils.JWTUtils;
+import com.attale.attale.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +41,9 @@ public class UserService implements IUserService {
             }
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             User savedUser=userRepository.save(user);
+            UserDTO userDTO= Utils.mapUserEntityToUserDTO(savedUser);
+            response.setStatusCode(200);
+            response.setUser(userDTO);
 
         }catch(OurException e){
             response.setStatusCode(400);
@@ -45,14 +51,39 @@ public class UserService implements IUserService {
 
         }catch(Exception e){
             response.setStatusCode(500);
-            response.setMessage("Error Occured During User Registration" +e.getMessage());
+            response.setMessage("Error Occurred During User Registration" +e.getMessage());
 
         }
-        return null;
+        return response;
     }
 
     @Override
     public Response login(LoginRequest loginRequest) {
+        Response response=new Response();
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword()));
+            var user =userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(()->new OurException("User not Found"));
+
+            var token=jwtUtils.generateToken(user);
+            response.setStatusCode(200);
+            response.setToken(token);
+            response.setRole(user.getRole());
+            response.setExpirationTime("7 Days");
+            response.setMessage("successful");
+
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        }catch(Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Login" +e.getMessage());
+
+
+        }
+
+
+
         return null;
     }
 
