c10a9e4c3f6fab650a767915084f0a999afdd7d2
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-10-28 22:05:08 +0530

    user service for getting user info using an email has been added

--- src/main/java/com/attale/attale/service/impl/UserService.java
+++ src/main/java/com/attale/attale/service/impl/UserService.java
@@ -181,7 +181,26 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public Response getMyInfo(String userId) {
-        return null;
+    public Response getMyInfo(String email) {
+        Response response=new Response();
+
+        try{
+            User user=userRepository.findByEmail(email).orElseThrow(()->new OurException("User Not Found"));
+            UserDTO userDTO=Utils.mapUserEntityToUserDTO(user);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUser(userDTO);
+
+        }catch (OurException e){
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        }catch(Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During Getting all Users" +e.getMessage());
+
+
+        }
+        return response;
     }
 }
