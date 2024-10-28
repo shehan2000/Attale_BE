c6e310024982694a25148b9eb1471bada071333b
shehan2000 <maliths.20@cse.mrt.ac.lk>
2024-10-28 23:04:11 +0530

    room service for adiing new room has been added

---
+++ src/main/java/com/attale/attale/service/impl/RoomService.java
@@ -0,0 +1,90 @@
+package com.attale.attale.service.impl;
+
+import com.attale.attale.dto.Response;
+import com.attale.attale.dto.RoomDTO;
+import com.attale.attale.entity.Room;
+import com.attale.attale.exception.OurException;
+import com.attale.attale.repo.BookingRepository;
+import com.attale.attale.repo.RoomRepository;
+import com.attale.attale.service.AwsS3Service;
+import com.attale.attale.service.interfac.IRoomService;
+import com.attale.attale.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class RoomService implements IRoomService {
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    private AwsS3Service awsS3Service;
+    @Override
+    public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
+        Response response=new Response();
+        try{
+            String imageUrl=awsS3Service.saveImageToS3(photo);
+            Room room=new Room();
+
+            room.setRoomPhotoUrl(imageUrl);
+            room.setRoomType(roomType);
+            room.setRoomPrice(roomPrice);
+            room.setRoomDescription(description);
+            Room savedRoom=roomRepository.save(room);
+            RoomDTO roomDTO= Utils.mapRoomEntityToRoomDTO(savedRoom);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoom(roomDTO);
+
+
+        }catch (Exception e){
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room"+e.getMessage());
+
+        }
+        return response;
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return null;
+    }
+
+    @Override
+    public Response getAllRooms() {
+        return null;
+    }
+
+    @Override
+    public Response deleteRoom(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response updateRoom(Long roomId, String roomType, BigDecimal roomPrice, MultipartFile photo) {
+        return null;
+    }
+
+    @Override
+    public Response getRoomById(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response getAvailableRoomsByDateAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        return null;
+    }
+
+    @Override
+    public Response getAllAvailableRooms() {
+        return null;
+    }
+}
